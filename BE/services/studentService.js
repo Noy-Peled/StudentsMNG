c680956118ecad031bc1ac0b2c104805fe85c911
noy peled <noypeled27@gmail.com>
2024-07-30 21:52:49 +0300

    add documentation

--- BE/services/studentService.js
+++ BE/services/studentService.js
@@ -1,22 +1,31 @@
 const studentModel = require("../models/studentModel");
 
+// find student by id from db, return student
 const getStudentById = async (id) => {
   const user = await studentModel.findById(id);
   return user;
 };
+
+// get all students from db, return students
 const getAllStudents = async () => {
   const users = await studentModel.find({});
   return users;
 };
+
+// creat new student by model and save it in db, return creation approval
 const createStudent = async (obj) => {
   const newUser = new studentModel(obj);
   await newUser.save();
   return "new user created";
 };
+
+// find student by id from db and update, return updating approval
 const updateStudent = async (id, obj) => {
   await studentModel.findByIdAndUpdate(id, obj);
   return "user updated";
 };
+
+// find student by id from db and delete, return deletion approval
 const deleteStudent = async (id) => {
   await studentModel.findByIdAndDelete(id);
   return "user deleted";
